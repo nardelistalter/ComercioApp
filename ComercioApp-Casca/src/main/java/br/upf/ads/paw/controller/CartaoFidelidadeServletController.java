b3edb894044f18a8f15768a672fc5aabc8ee63b5
Nardeli Miguel Stalter <nardelistalter@gmail.com>
2019-11-26 19:46:11 -0300

    Criando Form, List e ServletController de Cartão Fidelidade
    Replicando de Forma de Pagamento

---
+++ ComercioApp-Casca/src/main/java/br/upf/ads/paw/controller/CartaoFidelidadeServletController.java
@@ -0,0 +1,203 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.upf.ads.paw.controller;
+
+import br.upf.ads.paw.controladores.GenericDao;
+import br.upf.ads.paw.entidades.FormaPagamento;
+import br.upf.ads.paw.entidades.Permissao;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author pavan
+ */
+@WebServlet(name = "FormaPagamentoServletController", urlPatterns = {"/formaPagamento"})
+public class FormaPagamentoServletController extends HttpServlet {
+
+    GenericDao<FormaPagamento> daoFormaPagamento = new GenericDao(FormaPagamento.class);
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest req,
+            HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        Permissao p = Valida.acesso(req, resp, "FormaPagamento");
+
+        if (p == null) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/login?url=/formaPagamento");
+            dispatcher.forward(req, resp);
+        } else {
+            req.setAttribute("permissao", p);
+            String action = req.getParameter("searchAction");
+            if (action != null) {
+                switch (action) {
+                    case "searchById":
+                        searchById(req, resp);
+                        break;
+                    case "searchByName":
+                        if (p.getConsultar()) {
+                            searchByName(req, resp);
+                        } else {
+                            req.setAttribute("message", "Voc� n�o tem permiss�o para consultar.");
+                        }
+                        forwardList(req, resp, null);
+                }
+            } else {
+                List<FormaPagamento> result = null;
+                if (p.getConsultar()) {
+                    result = daoFormaPagamento.findEntities();
+                } else {
+                    req.setAttribute("message", "Voc� n�o tem permiss�o para consulrar");
+                }
+                forwardList(req, resp, result);
+            }
+        }
+    }
+
+    private void searchById(HttpServletRequest req,
+            HttpServletResponse resp)
+            throws ServletException, IOException {
+        long id = Integer.valueOf(req.getParameter("id"));
+        FormaPagamento obj = null;
+        try {
+            obj = daoFormaPagamento.findEntity(id);
+        } catch (Exception ex) {
+            Logger.getLogger(FormaPagamentoServletController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        req.setAttribute("obj", obj);
+        req.setAttribute("action", "edit");
+        String nextJSP = "/jsp/form-formaPagamento.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+        dispatcher.forward(req, resp);
+    }
+
+    private void searchByName(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String search = req.getParameter("search");
+        List<FormaPagamento> result = daoFormaPagamento.findEntitiesByField("descricao", search);  // buscar por nome
+        forwardList(req, resp, result);
+    }
+
+    private void forwardList(HttpServletRequest req, HttpServletResponse resp, List entityList)
+            throws ServletException, IOException {
+        String nextJSP = "/jsp/list-formaPagamento.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+        req.setAttribute("entities", entityList);
+        dispatcher.forward(req, resp);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest req,
+            HttpServletResponse resp)
+            throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if (action == null) {
+            doGet(req, resp);
+        }
+        switch (action) {
+            case "add":
+                addAction(req, resp);
+                break;
+            case "edit":
+                editAction(req, resp);
+                break;
+            case "remove":
+                removeById(req, resp);
+                break;
+        }
+    }
+
+    private void addAction(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        try {
+            String descricao = req.getParameter("descricao");
+
+            FormaPagamento obj = new FormaPagamento(null, descricao);
+            daoFormaPagamento.create(obj);
+            long id = obj.getId();
+            req.setAttribute("id", id);
+            String message = "Um novo registro foi criado com sucesso.";
+            req.setAttribute("message", message);
+            doGet(req, resp);
+        } catch (Exception ex) {
+            Logger.getLogger(FormaPagamentoServletController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void editAction(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String descricao = req.getParameter("descricao");
+        long id = Integer.valueOf(req.getParameter("id"));
+        FormaPagamento obj = new FormaPagamento(id, descricao);
+        boolean success = false;
+        try {
+            daoFormaPagamento.edit(obj);
+            success = true;
+        } catch (Exception ex) {
+            Logger.getLogger(FormaPagamentoServletController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String message = null;
+        if (success) {
+            message = "O registro foi atualizado com sucesso";
+        }
+        req.setAttribute("id", obj.getId());
+        req.setAttribute("message", message);
+        doGet(req, resp);
+    }
+
+    private void removeById(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        long id = Integer.valueOf(req.getParameter("id"));
+        boolean confirm = false;
+        try {
+            daoFormaPagamento.destroy(id);
+            confirm = true;
+        } catch (Exception ex) {
+            Logger.getLogger(FormaPagamentoServletController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (confirm) {
+            String message = "O registro foi removido com sucesso.";
+            req.setAttribute("message", message);
+        }
+        doGet(req, resp);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ ComercioApp-Casca/src/main/webapp/jsp/list-cartaoFidelidade.jsp
@@ -0,0 +1,78 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@include file="../header.jspf" %>
+<div class="container">
+    <h2>Formas de Pagamento</h2>
+    <!--Search Form -->
+    <form action="/formaPagamento" method="get" id="searchFormaPagamentoForm" role="form">
+        <input type="hidden" id="searchAction" name="searchAction" value="searchByName">
+        <div class="form-group-lg col-xs-6">
+            <input type="text" name="search" id="search" class="form-control" required="true" placeholder="Digite a descrição da forma de pagamento a procurar"/>                    
+        </div>
+        <button type="submit" class="btn btn-info">
+            <span class="glyphicon glyphicon-search"></span> Procurar
+        </button>
+        <br></br>
+        <br></br>
+    </form>
+
+    <!-- Include Botton -->
+    <form action ="jsp/form-formaPagamento.jsp">            
+        <c:if test="${permissao.getCriar()}">
+            <button type="submit" class="btn btn-primary  btn-md">Novo Cadastro</button> 
+        </c:if>
+        <br></br>
+    </form>
+
+    <!-- List-->
+    <c:if test="${not empty message}">                
+        <div class="alert alert-success">
+            ${message}
+        </div>
+    </c:if> 
+    <form action="/formaPagamento" method="post" id="formaPagamentoForm" role="form" >              
+        <input type="hidden" id="id" name="id">
+        <input type="hidden" id="action" name="action">
+        <c:choose>
+            <c:when test="${not empty entities}">
+                <table  class="table-striped table-xl col-xs-10">
+                    <thead>
+                        <tr>
+                            <td>#</td>
+                            <td>Descrição</td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <c:forEach var="obj" items="${entities}">
+                        <tr class="${id == obj.id?"info":""}">
+                            <td>
+                                <c:if test="${permissao.getAlterar()}">
+                                    <a href="/formaPagamento?id=${obj.id}&searchAction=searchById">${obj.id}</a>
+                                </c:if>
+                                <c:if test="${!permissao.getAlterar()}">
+                                    ${obj.id}
+                                </c:if>
+                            </td>                                    
+                            <td>${obj.descricao}</td>
+                            <td> <td> 
+                                <c:if test="${permissao.getExcluir()}">
+                                    <a href="#" id="remove" 
+                                       onclick="document.getElementById('action').value = 'remove';document.getElementById('id').value = '${obj.id}';
+                                               document.getElementById('formaPagamentoForm').submit();"> 
+                                        <span class="glyphicon glyphicon-trash"/>
+                                    </a>
+                                </c:if>
+                            </td>
+                        </tr>
+                    </c:forEach>               
+                </table>  
+            </c:when>                    
+            <c:otherwise>
+                <br>           
+                <div class="alert alert-info">
+                    Nenhum registro encontrado.
+                </div>
+            </c:otherwise>
+        </c:choose>                        
+    </form>
+</div>
+<%@include file="../footer.jspf" %>
